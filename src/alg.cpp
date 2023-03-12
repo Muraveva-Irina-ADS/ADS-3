6cc6f7371a56200fc911a812384535e66e8f71c4
Muraveva-Irina-ADS <124904325+Muraveva-Irina-ADS@users.noreply.github.com>
2023-03-12 18:34:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,8 +2,29 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
+    int first = 0, end = size-1, curr;
     int count = 0;
-    for (int i = 0; i < size; i++)
-        if (*(arr + i) == value) count++;
+    while (first != end) {
+        curr = (first + end) / 2;
+        if (arr[curr] == value) {
+            count++;
+            int cur_1 = curr+1;
+            int cur_2 = curr-1;
+            while (arr[cur_1] == value) {
+                count++;
+                cur_1++;
+            }
+            while (arr[cur_2] == value) {
+                count++;
+                cur_2--;
+            }
+            first = end;
+        }
+        else
+            if (arr[curr] > value)
+                end = curr;
+        else
+            first = curr+1;
+    }
     return count;
 }
